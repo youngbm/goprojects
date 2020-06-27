afe0736fe1d4b5ba0fcda7e1e13ec6f513d593e1
youngbm <young.bm@163.com>
2020-06-28 02:32:40 +0800

    io

--- stdpackage/bufio/fileop.go
+++ stdpackage/bufio/fileop.go
@@ -10,14 +10,11 @@ import (
 
 func main() {
 	fmt.Println("main")
-	//fmt.Printf("%v  /  %T\n", wString, wString)
 	var (
-		wString  = "测试字符串\n"
 		filename = "/tmp/test284057930"
 		f        *os.File
 		err      error
 	)
-
 	//fInfo, err := os.Stat(filename)
 	//if err != nil {
 	//	fmt.Println(err)
@@ -37,25 +34,33 @@ func main() {
 		log.Fatal(err)
 	}
 	//  io 模块去写 string
-	_, err = io.WriteString(f, wString)
+	_, err = io.WriteString(f, "io\n")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("os fd + io write")
 
 	//打开的文件句柄  file.WriteString(wstring)
-	_, err = f.WriteString(wString)
+	_, err = f.Write([]byte("fd write\n"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("os fd + fd write(os single)")
 
-	// bufio 模块写
+	//打开的文件句柄  file.WriteString(wstring)
+	_, err = f.WriteString("fd writestring\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("os fd + fd writeString(os single)")
+
+	// bufio 模块写 需要Flush
 	w := bufio.NewWriter(f)
-	_, err = w.WriteString(wString)
+	_, err = w.WriteString("fd + bufid\n")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("os fd + bufio ")
-
+	w.Flush()
+	f.Close()
 }
